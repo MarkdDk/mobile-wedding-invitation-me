3aefcd105c6a6044467b396267fb07f5aa0087a4
Daeguk Do <daeguk.do@roboetech.com>
2026-05-01 20:48:59 +0900

    test3

---
+++ src/layout/Gallery/Images_.ts
@@ -0,0 +1,94 @@
+import image01 from '@/assets/images/1.jpg'
+import image02 from '@/assets/images/2.jpg'
+import image03 from '@/assets/images/3.jpg'
+import image04 from '@/assets/images/4.jpg'
+import image05 from '@/assets/images/5.jpg'
+import image06 from '@/assets/images/6.jpg'
+import image07 from '@/assets/images/7.jpg'
+import image08 from '@/assets/images/8.jpg'
+import image09 from '@/assets/images/9.jpg'
+import image10 from '@/assets/images/10.jpg'
+import image11 from '@/assets/images/11.jpg'
+import image12 from '@/assets/images/12.jpg'
+import image13 from '@/assets/images/13.jpg'
+import image14 from '@/assets/images/14.jpg'
+
+const galleryImageSize = {
+  width: 1280,
+  height: 1920,
+}
+
+const images = [
+  {
+    alt: 'image01',
+    source: image01,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image02',
+    source: image02,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image03',
+    source: image03,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image04',
+    source: image04,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image05',
+    source: image05,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image06',
+    source: image06,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image07',
+    source: image07,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image08',
+    source: image08,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image09',
+    source: image09,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image10',
+    source: image10,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image11',
+    source: image11,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image12',
+    source: image12,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image13',
+    source: image13,
+    ...galleryImageSize,
+  },
+  {
+    alt: 'image14',
+    source: image14,
+    ...galleryImageSize,
+  },
+]
+
+export default images
