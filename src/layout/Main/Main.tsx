288bc74351dcd27ba238253d5d06f8dcd7441aa7
Daeguk Do <daeguk.do@roboetech.com>
2026-05-16 15:49:05 +0900

    edit 1

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import data from 'data.json';
-import mainImg from '@/assets/images/1.jpg'
+import mainImg from '@/assets/images/0.jpg'
 
 const Main = () => {
   const { greeting } = data;
